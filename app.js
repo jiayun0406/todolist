e67940082c1ace559d2c2c252355f2c2f8912138
jiayun0406 <ask16880@gmail.com>
2021-06-11 21:16:07 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("button")
+let input = document.querySelector("input")
+let ul = document.querySelector("ul")
+btn.addEventListener("click", function () {
+  let li = document.createElement("li")
+  li.innerText = input.value
+  ul.append(li)
+})
